2035c18e5a90c8f9532e0ec2b6cf8270e4733285
ianspractice <bodybody.ian@gmail.com>
2025-05-22 20:19:35 -0600

    Iterates through one complete round

--- bubble.rb
+++ bubble.rb
@@ -1,11 +1,30 @@
+# def bubble_sort(array)
+#   first = array[0]
+#   second = array[1]
+#   if first > second
+#     array[1] = first
+#     array[0] = second
+#     p array
+#   end
+# end
+
+
 #get first element of the array
 def bubble_sort(array)
-  first = array[0]
-  second = array[1]
-  if first > second
-    array[1] = first
-    array[0] = second
-    p array
+  length = array.length
+  while length > 0
+    array.map do |value|
+      first_value = array[array.index(value)]
+      second_value = array[array.index(value) + 1]
+        if second_value == nil
+          return array
+        elsif first_value > second_value
+        array[array.index(value)], array[array.index(value) + 1] = array[array.index(value) + 1], array[array.index(value)]
+        length -= 1
+        else
+          length -= 1
+        end
+    end
   end
 end
 #get second element of the array
@@ -20,4 +39,4 @@ end
 
 #return new array
 
-bubble_sort([2, 1])
+p bubble_sort([19, 2, 18, 3, 5, 7])
